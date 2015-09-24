c4be7d9c553a6b6a80d33167c7544e43493f618b
Dieter Van de Walle <dieter.vandewalle@lightspeedpos.com>
2015-09-25 00:07:27 +0200

    Pretty much everything is working.

--- LFO-ADSR.ino
+++ LFO-ADSR.ino
@@ -1,3 +1,4 @@
+#define TRIGGER_INPUT_PIN 0
 #define VALUE_INPUT_PIN A0
 #define PARAMETER_INPUT_PIN A1
 #define LFO_OUTPUT_PIN 5
@@ -15,73 +16,136 @@
 #define ADSR_DECAY_PARAMETER_VALUE 6// 99
 #define ADSR_SUSTAIN_PARAMETER_VALUE 8 // 133
 #define ADSR_RELEASE_PARAMETER_VALUE 9 // 148
-#define ADSR_U1_PARAMETER_VALUE 11 // 181 // Unused for now
-#define ADSR_U2_PARAMETER_VALUE 13 // 217 // Unused for now
-#define ADSR_REPEAT_PARAMETER_VALUE 15 // 249
-#define ADSR_SYNC_PARAMETER_VALUE 16 // 265
+#define ADSR_OFFSET_PARAMETER_VALUE 11 // 181 // Unused for now
+#define ADSR_WIDTH_PARAMETER_VALUE 13 // 217 // Unused for now
+#define ADSR_INVERT_PARAMETER_VALUE 15 // 249
+#define ADSR_REPEAT_PARAMETER_VALUE 16 // 265
 
-#define UPDATE_INTERVAL 10 // ms
+#define UPDATE_INTERVAL 100 // microseconds
 
-#define PARAM_READ_INTERVAL 500 // ms
-long lastParamTime;
+#define PARAM_READ_INTERVAL 10 // ms
+unsigned long lastParamTime;
 
 // LFO parameters
-int lfo_rate;
-int lfo_shape;
-int lfo_offset;
-int lfo_width;
+#define LFO_FREQ_LOW 1000 // micros
+#define LFO_FREQ_HIGH 1000000 // micros
+int lfo_shape = 512;
+int lfo_offset = 0;
+int lfo_width = 255;
 boolean lfo_sync;
-
-// Derived values
-#define LFO_FREQ_LOW 10 // ms
-#define LFO_FREQ_HIGH 10000 // ms
-long lfoStart = 0;
-int lfoPeriodLength;
+unsigned long lfoStart = 0;
+unsigned long lfoPeriodLength = 500000; 
 
 // ADSR parameters
-int adsr_attack;
-int adsr_decay;
-int adsr_sustain;
-int adsr_release;
-boolean adsr_repeat;
-boolean adsr_sync;
+#define ADSR_TIME_LOW 1000 // micros
+#define ADSR_TIME_HIGH 10000000 // micros
+#define ADSR_TRIGGER_UPDATE_INTERVAL 1000 // micros
+int adsr_offset = 0;
+int adsr_width = 255;
+boolean adsr_invert = false;
+boolean adsr_repeat = false;
+unsigned long adsrAttackTime = 500000;
+unsigned long adsrDecayTime = 500000;
+unsigned long adsrSustainLevel = 255;
+unsigned long adsrReleaseTime = 500000;
+int adsrStartValue;
+int adsrValue;
+
+// Trigger
+unsigned long lastTriggerUpdateTime = 0;
+unsigned long lastTriggerTime = 0;
+boolean trigger;
 
 void setup() {
-  pinMode(LFO_OUTPUT_PIN, OUTPUT);
-  pinMode(ADSR_OUTPUT_PIN, OUTPUT);
-  pinMode(VALUE_INPUT_PIN, INPUT);
-  pinMode(PARAMETER_INPUT_PIN, INPUT);
+  pinMode(TRIGGER_INPUT_PIN, INPUT);
+  analogWrite(ADSR_OUTPUT_PIN, 0);
 }
 
 void loop() {
-  long curTime = millis();
+  unsigned long curTime = micros();
 
   // Read parameter values every PARAM_READ_INTERVAL ms
-  if (curTime > lastParamTime + PARAM_READ_INTERVAL) {
+  if (curTime / 1000 > lastParamTime + PARAM_READ_INTERVAL) {
     readParameter();
-    lastParamTime = curTime;
+    lastParamTime = curTime / 1000;
   }
 
-  // Set LFO voltage
-  adjustLFO(curTime);
-
+  // Check trigger
+  if (curTime > lastTriggerUpdateTime + ADSR_TRIGGER_UPDATE_INTERVAL) {
+    boolean triggerVal = digitalRead(TRIGGER_INPUT_PIN);
+    if (triggerVal != trigger) {
+      // Trigger changed!
+      trigger = triggerVal;
+      lastTriggerTime = curTime;
+    }
+    
+    lastTriggerUpdateTime = curTime;
+  }
+  
   // Set ADSR voltage
   adjustADSR(curTime);
-  
-  delay(UPDATE_INTERVAL - (curTime % UPDATE_INTERVAL));
+
+    // Set LFO voltage
+  adjustLFO(curTime);
+
+  delayMicroseconds(UPDATE_INTERVAL - (curTime % UPDATE_INTERVAL));
 }
 
 void adjustLFO(long curTime) {
-  int position = (curTime - lfoStart) % lfoPeriodLength;
+  long position = (lfo_sync ? (curTime - lastTriggerTime) : curTime) % lfoPeriodLength;
 
-  // Only saw wave for now
-  int value = map(position, 0, lfoPeriodLength, 0, 256);
+  long peakPosition = map(lfo_shape, 0, 1024, 0, lfoPeriodLength);
+  int value;
+  if (position < peakPosition) {
+     // Ramping up
+    value = map(position, 0, peakPosition, 0, 256);
+  } else {
+    // Ramping down
+    value = map(position, peakPosition, lfoPeriodLength, 256, 0);
+  }
+
+  value = map(value, 0, 255, 0, lfo_width); // Scale for width
+  value = map(value, 0, 255, lfo_offset, 256); // Adjust for offset
   
   analogWrite(LFO_OUTPUT_PIN, value);
-}
+} 
 
 void adjustADSR(long curTime) {
-  
+  long position = curTime - lastTriggerTime;
+
+  if (position == 0) {
+    adsrStartValue = adsrValue;
+  }
+  if (adsr_repeat && trigger) {
+    position %= (adsrAttackTime+adsrDecayTime+adsrReleaseTime);
+  }
+
+  if (trigger) {
+    // Trigger on
+    if (position < adsrAttackTime) {
+      adsrValue = map(position, 0, adsrAttackTime, adsrStartValue, 256);
+    } else if (position < (adsrAttackTime + adsrDecayTime)) {
+      adsrValue = map(position - adsrAttackTime, 0, adsrDecayTime, 255, adsrSustainLevel);
+    } else if (adsr_repeat && position < (adsrAttackTime+adsrDecayTime+adsrReleaseTime))  {
+        adsrValue = map(position-adsrAttackTime-adsrDecayTime, 0, adsrReleaseTime, adsrSustainLevel, 0); // Release
+    } else {
+        adsrValue = adsrSustainLevel;
+    }
+  } else {
+    // Trigger off
+    if (position < adsrReleaseTime) {
+      adsrValue = map(position, 0, adsrReleaseTime, adsrStartValue, 0);
+    } else {
+      adsrValue = 0;
+    }
+  }
+
+  int value = map(adsrValue, 0, 255, 0, adsr_width); // Scale for width
+  value = map(value, 0, 255, adsr_offset, 255); // Adjust for offset
+  if (adsr_invert) {
+    value = 255 - value;
+  }
+  analogWrite(ADSR_OUTPUT_PIN, value);
 }
 
 void readParameter() {
@@ -98,62 +162,65 @@ void handleParameter(int paramChoice) {
 
   switch (quantChoice) {
     case LFO_RATE_PARAMETER_VALUE:
-      lfo_rate = paramValue;
-      lfoPeriodLength = map(lfo_rate, 0, 1024, LFO_FREQ_LOW, LFO_FREQ_HIGH); 
+      lfoPeriodLength = map(paramValue, 0, 1024, LFO_FREQ_LOW, LFO_FREQ_HIGH); 
       break;
     case LFO_SHAPE_PARAMETER_VALUE:
       lfo_shape = paramValue;
       break;
     case LFO_OFFSET_PARAMETER_VALUE:
-      lfo_offset = paramValue;
+      lfo_offset = map(paramValue, 0, 1024, 0, 256);
       break;
     case LFO_WIDTH_PARAMETER_VALUE:
-      lfo_width = paramValue;
+      lfo_width = map(paramValue, 0, 1024, 0, 256);
       break;
     case LFO_SYNC_PARAMETER_VALUE:
       lfo_sync = mapBoolean(paramValue);
       break;
     case ADSR_ATTACK_PARAMETER_VALUE:
-      adsr_attack = paramValue;
+      adsrAttackTime = map(paramValue, 0, 1024, ADSR_TIME_LOW, ADSR_TIME_HIGH);
       break;
     case ADSR_DECAY_PARAMETER_VALUE:
-      adsr_decay = paramValue;
+      adsrDecayTime = map(paramValue, 0, 1024, ADSR_TIME_LOW, ADSR_TIME_HIGH);
       break;
     case ADSR_SUSTAIN_PARAMETER_VALUE:
-      adsr_sustain = paramValue;
+      adsrSustainLevel = map(paramValue, 0, 1024, 0, 256);
       break;
     case ADSR_RELEASE_PARAMETER_VALUE:
-      adsr_release = paramValue;
+      adsrReleaseTime = map(paramValue, 0, 1024, ADSR_TIME_LOW, ADSR_TIME_HIGH);
       break;
-    case ADSR_U1_PARAMETER_VALUE:
-    case ADSR_U2_PARAMETER_VALUE:
-      // Unused for now
+    case ADSR_OFFSET_PARAMETER_VALUE:
+      adsr_offset = map(paramValue, 0, 1024, 0, 256);
+      break;
+    case ADSR_WIDTH_PARAMETER_VALUE:
+      adsr_width = map(paramValue, 0, 1024, 0, 256);
+      break;
+    case ADSR_INVERT_PARAMETER_VALUE:
+      adsr_invert = mapBoolean(paramValue);
       break;
     case ADSR_REPEAT_PARAMETER_VALUE:
       adsr_repeat = mapBoolean(paramValue);
-      break;
-    case ADSR_SYNC_PARAMETER_VALUE:
-      adsr_sync = mapBoolean(paramValue);
       break;
     default:
       Serial.print("Mapping parameter failed!: ");
       Serial.println(quantChoice);
   }
 
-  Serial.print("LFO: ");
-  Serial.print(lfo_rate); Serial.print(" ");
-  Serial.print(lfo_shape); Serial.print(" ");
-  Serial.print(lfo_offset); Serial.print(" ");
-  Serial.print(lfo_width); Serial.print(" ");
+  Serial.print("LFO: P:");
+  Serial.print(lfoPeriodLength/1000); Serial.print("ms S:");
+  Serial.print(lfo_shape); Serial.print(" O:");
+  Serial.print(lfo_offset); Serial.print(" W:");
+  Serial.print(lfo_width); Serial.print(" S:");
   Serial.println(lfo_sync);
   
-  Serial.print("ADSR: ");
-  Serial.print(adsr_attack); Serial.print(" ");
-  Serial.print(adsr_decay); Serial.print(" ");
-  Serial.print(adsr_sustain); Serial.print(" ");
-  Serial.print(adsr_release); Serial.print(" ");
-  Serial.print(adsr_repeat); Serial.print(" ");
-  Serial.println(adsr_sync);
+  Serial.print("ADSR: A:");
+  Serial.print(adsrAttackTime/1000); Serial.print("ms D:");
+  Serial.print(adsrDecayTime/1000); Serial.print("ms S:");
+  Serial.print(adsrSustainLevel); Serial.print(" R:");
+  Serial.print(adsrReleaseTime/1000); Serial.print("ms O:");
+  Serial.print(adsr_offset); Serial.print(" W:");
+  Serial.print(adsr_width); Serial.print(" I:");
+  Serial.print(adsr_invert); Serial.print(" R:");
+  Serial.println(adsr_repeat);
 }
 
 boolean mapBoolean(int paramValue) {
