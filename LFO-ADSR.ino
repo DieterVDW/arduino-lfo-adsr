c75c21fd6ae222e4888b1202fb0b152fdfa4300d
Dieter Van de Walle <dieter.vandewalle@lightspeedpos.com>
2015-09-25 11:32:03 +0200

    Better fix for overflow problem + add sloping of time parameters.

--- LFO-ADSR.ino
+++ LFO-ADSR.ino
@@ -27,18 +27,20 @@
 unsigned long lastParamTime;
 
 // LFO parameters
-#define LFO_FREQ_LOW 1000 // micros
-#define LFO_FREQ_HIGH 1000000 // micros
+#define LFO_PERIOD_MIN 1000 // micros
+#define LFO_PERIOD_MAX 1000000 // micros
+#define LFO_PERIOD_SLOPE 1.0
 int lfo_shape = 512;
 int lfo_offset = 0;
 int lfo_width = 255;
 boolean lfo_sync;
 unsigned long lfoStart = 0;
-unsigned long lfoPeriodLength = 500000; 
+unsigned long lfoPeriodLength = 500000;
 
 // ADSR parameters
-#define ADSR_TIME_LOW 1000 // micros
-#define ADSR_TIME_HIGH 10000000 // micros
+#define ADSR_TIME_MIN 1000 // micros
+#define ADSR_TIME_MAX 10000000 // micros
+#define ADSR_SLOPE 1.0
 #define ADSR_TRIGGER_UPDATE_INTERVAL 1000 // micros
 int adsr_offset = 0;
 int adsr_width = 255;
@@ -78,14 +80,14 @@ void loop() {
       trigger = triggerVal;
       lastTriggerTime = curTime;
     }
-    
+
     lastTriggerUpdateTime = curTime;
   }
-  
+
   // Set ADSR voltage
   adjustADSR(curTime);
 
-    // Set LFO voltage
+  // Set LFO voltage
   adjustLFO(curTime);
 
   delayMicroseconds(UPDATE_INTERVAL - (curTime % UPDATE_INTERVAL));
@@ -112,9 +114,9 @@ void adjustLFO(long curTime) {
 
   value = map(value, 0, 255, 0, lfo_width); // Scale for width
   value = map(value, 0, 255, lfo_offset, 256); // Adjust for offset
-  
+
   analogWrite(LFO_OUTPUT_PIN, value);
-} 
+}
 
 void adjustADSR(long curTime) {
   long position = curTime - lastTriggerTime;
@@ -123,7 +125,7 @@ void adjustADSR(long curTime) {
     adsrStartValue = adsrValue;
   }
   if (adsr_repeat && trigger) {
-    position %= (adsrAttackTime+adsrDecayTime+adsrReleaseTime);
+    position %= (adsrAttackTime + adsrDecayTime + adsrReleaseTime);
   }
 
   if (trigger) {
@@ -132,10 +134,10 @@ void adjustADSR(long curTime) {
       adsrValue = map(position, 0, adsrAttackTime, adsrStartValue, 256);
     } else if (position < (adsrAttackTime + adsrDecayTime)) {
       adsrValue = map(position - adsrAttackTime, 0, adsrDecayTime, 255, adsrSustainLevel);
-    } else if (adsr_repeat && position < (adsrAttackTime+adsrDecayTime+adsrReleaseTime))  {
-        adsrValue = map(position-adsrAttackTime-adsrDecayTime, 0, adsrReleaseTime, adsrSustainLevel, 0); // Release
+    } else if (adsr_repeat && position < (adsrAttackTime + adsrDecayTime + adsrReleaseTime))  {
+      adsrValue = map(position - adsrAttackTime - adsrDecayTime, 0, adsrReleaseTime, adsrSustainLevel, 0); // Release
     } else {
-        adsrValue = adsrSustainLevel;
+      adsrValue = adsrSustainLevel;
     }
   } else {
     // Trigger off
@@ -168,7 +170,7 @@ void handleParameter(int paramChoice) {
 
   switch (quantChoice) {
     case LFO_RATE_PARAMETER_VALUE:
-      lfoPeriodLength = map(paramValue, 0, 1024, LFO_PERIOD_LOW, LFO_PERIOD_HIGH); 
+      lfoPeriodLength = slopeMap(paramValue, LFO_PERIOD_MIN, LFO_PERIOD_MAX, LFO_PERIOD_SLOPE);
       break;
     case LFO_SHAPE_PARAMETER_VALUE:
       lfo_shape = paramValue;
@@ -183,16 +185,16 @@ void handleParameter(int paramChoice) {
       lfo_sync = mapBoolean(paramValue);
       break;
     case ADSR_ATTACK_PARAMETER_VALUE:
-      adsrAttackTime = map(paramValue, 0, 1024, ADSR_TIME_LOW, ADSR_TIME_HIGH);
+      adsrAttackTime = slopeMap(paramValue, ADSR_TIME_MIN, ADSR_TIME_MAX, ADSR_SLOPE);
       break;
     case ADSR_DECAY_PARAMETER_VALUE:
-      adsrDecayTime = map(paramValue, 0, 1024, ADSR_TIME_LOW, ADSR_TIME_HIGH);
+      adsrDecayTime = slopeMap(paramValue, ADSR_TIME_MIN, ADSR_TIME_MAX, ADSR_SLOPE);
       break;
     case ADSR_SUSTAIN_PARAMETER_VALUE:
       adsrSustainLevel = map(paramValue, 0, 1024, 0, 256);
       break;
     case ADSR_RELEASE_PARAMETER_VALUE:
-      adsrReleaseTime = map(paramValue, 0, 1024, ADSR_TIME_LOW, ADSR_TIME_HIGH);
+      adsrReleaseTime = slopeMap(paramValue, ADSR_TIME_MIN, ADSR_TIME_MAX, ADSR_SLOPE);
       break;
     case ADSR_OFFSET_PARAMETER_VALUE:
       adsr_offset = map(paramValue, 0, 1024, 0, 256);
@@ -212,17 +214,17 @@ void handleParameter(int paramChoice) {
   }
 
   Serial.print("LFO: P:");
-  Serial.print(lfoPeriodLength/1000); Serial.print("ms S:");
+  Serial.print(lfoPeriodLength / 1000); Serial.print("ms S:");
   Serial.print(lfo_shape); Serial.print(" O:");
   Serial.print(lfo_offset); Serial.print(" W:");
   Serial.print(lfo_width); Serial.print(" S:");
   Serial.println(lfo_sync);
-  
+
   Serial.print("ADSR: A:");
-  Serial.print(adsrAttackTime/1000); Serial.print("ms D:");
-  Serial.print(adsrDecayTime/1000); Serial.print("ms S:");
+  Serial.print(adsrAttackTime / 1000); Serial.print("ms D:");
+  Serial.print(adsrDecayTime / 1000); Serial.print("ms S:");
   Serial.print(adsrSustainLevel); Serial.print(" R:");
-  Serial.print(adsrReleaseTime/1000); Serial.print("ms O:");
+  Serial.print(adsrReleaseTime / 1000); Serial.print("ms O:");
   Serial.print(adsr_offset); Serial.print(" W:");
   Serial.print(adsr_width); Serial.print(" I:");
   Serial.print(adsr_invert); Serial.print(" R:");
@@ -231,5 +233,14 @@ void handleParameter(int paramChoice) {
 
 boolean mapBoolean(int paramValue) {
   return map(paramValue, 0, 1024, 0, 2) == 1;
+}
+
+/*
+ * Own map() implementation that supports big long's and slope.
+ */
+long slopeMap(long paramValue, long min, long max, float slope) {
+  float index = (float)paramValue/1024;
+  paramValue -= (1 - pow(index, 2)) * 1024 * slope; // Compensate for slope
+  return (max / 1024) * paramValue;
 }
 
